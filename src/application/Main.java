7b9efb35e7143a0481dc486213d67a95be324200
KamilaSouza <kamisouzas@gmail.com>
2022-01-25 09:07:38 -0300

    First Class Position

---
+++ src/application/Main.java
@@ -0,0 +1,12 @@
+package application;
+
+import boardgame.Position;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Position position = new Position(3,5);
+        System.out.println(position);
+    }
+}
